bd230b82946f485fba5ddcb4a411bee148f339ed
Dayshawn Terry <Dayshawn21@gmail.com>
2020-09-15 21:39:02 -0500

    build

---
+++ build/precache-manifest.c37fe4a326d56a6b911438b10fcdfbeb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b383d8b6f093617a37192d422d1588dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a054ab5f7532146ff66d",
+    "url": "/static/css/main.9595d6a2.chunk.css"
+  },
+  {
+    "revision": "b1953a7cd903259897c3",
+    "url": "/static/js/2.5635efc6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5635efc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a054ab5f7532146ff66d",
+    "url": "/static/js/main.d2e68a9f.chunk.js"
+  },
+  {
+    "revision": "8db1d3d8fdface5ac500",
+    "url": "/static/js/runtime-main.24186bf3.js"
+  },
+  {
+    "revision": "fa63562dec20d4f25fe3e0b4005c8cca",
+    "url": "/static/media/DGOLD_BLACK_720x.fa63562d.png"
+  },
+  {
+    "revision": "e3b3f51b0cff63996b8080a995820b5e",
+    "url": "/static/media/gih.e3b3f51b.jpg"
+  },
+  {
+    "revision": "16cd2323b7e894e2744ceccd2c15fff8",
+    "url": "/static/media/lg.16cd2323.jpg"
+  },
+  {
+    "revision": "4b24be18ae0c9b5baa846499b8ef9ea9",
+    "url": "/static/media/pexels-andrew-neel-2312369 (1).4b24be18.jpg"
+  },
+  {
+    "revision": "000a41da2e3536f53da6f18cc4238988",
+    "url": "/static/media/pexels-ivan-bertolazzi-2681319.000a41da.jpg"
+  },
+  {
+    "revision": "964bec6fa2eb2c1fcab586c4967a0335",
+    "url": "/static/media/tw.964bec6f.jpg"
+  },
+  {
+    "revision": "34c5765ec4abcfacc12e717f87a0ce8a",
+    "url": "/static/media/wf.34c5765e.jpg"
+  }
+]);
